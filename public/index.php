4c005afc8e61aee657a7ba222e81a9d2aa1de7ce
Flavio Silva <flv.alves@gmail.com>
2015-07-30 22:07:41 -0300

    criando rota clientes e retornando um json

--- public/index.php
+++ public/index.php
@@ -1,6 +1,13 @@
 <?php
 require __DIR__ . '/../bootstrap.php';
 
+$clientes = [
+	['nome' => 'Flavio', 'email' => 'flv.alves@gmail.com', 'cpf' => '999.999.999-99'],
+	['nome' => 'Mayara', 'email' => 'mayarachagas2010@gmail.com', 'cpf' => '888.888.888-88'],
+];
 
+$app->get('/clientes', function(\Silex\Application $app) use($clientes) {
+	return $app->json($clientes);
+});
 
 $app->run();
