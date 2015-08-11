55910ef409cd83ff37910b0dbf6a7c464918488d
Flavio Silva <flv.alves@gmail.com>
2015-08-11 19:35:59 -0300

    disponibilizando uma api rest com o método GET para listar um produto e todos

--- public/index.php
+++ public/index.php
@@ -84,8 +84,33 @@ $product->get('delete/{id}', function (\Silex\Application $app, $id) {
     $app['product.service']->delete($id);
     return $app->redirect('/product');
 })->bind('delete');
-        
+
+$products = $app['controllers_factory'];
+
+$products->get('/', function (Application $app) {
+    foreach($app['product.service']->findAll() as $product) {
+        $data[$product->getId()] = $product->toArray();
+    }
+    
+    return $app->json($data);
+});
+
+$products->get('/{id}', function (Application $app, $id){
+    $product = $app['product.service']->findBy($id);
+    return $app->json($product->toArray());
+});
+
+$products->post('/', function (Application $app, Request $request){
+   $data['name'] = $request->get('name');
+   $data['description'] = $request->get('description');
+   $data['value'] = $request->get('value');
+   $product = $app['product.service']->save($data);
+   
+   return $app->json($product->toArray());
+});
+
 /*@var $app \Silex\Application*/
 $app->mount('/product', $product);
 $app->mount('/client', $client);
+$app->mount('/products', $products);
 $app->run();
